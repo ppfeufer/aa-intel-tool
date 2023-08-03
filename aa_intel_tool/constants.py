45d6880798ca1df456fefbba730c488b8f9fce49
Peter Pfeufer <info@ppfeufer.de>
2023-08-03 22:50:45 +0200

    [CHANGE] Regex patterns improved

--- aa_intel_tool/constants.py
+++ aa_intel_tool/constants.py
@@ -1,6 +1,7 @@
 """
 App constants
 """
+
 # Standard Library
 import re
 
@@ -11,12 +12,14 @@ SUPPORTED_INTEL_TYPES = [
     {
         "name": "Fleet Composition",
         "parser": "fleetcomp",
-        "pattern": re.compile(r"(?im)^([a-zA-Z0-9 -_]{3,37})[\t](.*)[\t](.* / .*) ?$"),
+        "pattern": re.compile(
+            r"(?im)^([a-zA-Z0-9 -_]{3,37})[\t](.*)[\t](.*)[\t](.*)[\t](.*)[\t]([0-5] - [0-5] - [0-5])[\t](.*) ?$"  # pylint: disable=line-too-long
+        ),
     },
     {
         "name": "D-Scan",
         "parser": "dscan",
-        "pattern": re.compile(r"(?im)^\d+[\t](.*)[\t](-|\d(.*)) ?$"),
+        "pattern": re.compile(r"(?im)^(\d+)[\t](.*)[\t](.*)[\t](-|(.*) (km|AU)) ?$"),
     },
     {
         "name": "Chat List",
