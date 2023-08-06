bcc8c1a2f7cf00b075fdfde0638ddc3ddd41c3a7
Peter Pfeufer <info@ppfeufer.de>
2023-08-06 18:03:34 +0200

    [CHANGE] Scan data handling refactored
    - Scan data split in sections and stored separately in the DB
    - All relevant information added to jSon data
    - Rendering done by Datatables jSon functionality

--- aa_intel_tool/parser/chatlist.py
+++ aa_intel_tool/parser/chatlist.py
@@ -3,6 +3,7 @@ Chat list parser
 """
 
 # Alliance Auth
+from allianceauth.eveonline.evelinks import dotlan, eveimageserver, evewho, zkillboard
 from allianceauth.services.hooks import get_extension_logger
 
 # Alliance Auth (External Libs)
@@ -17,7 +18,9 @@ from aa_intel_tool.models import Scan
 logger = LoggerAddTag(my_logger=get_extension_logger(name=__name__), prefix=__title__)
 
 
-def parse(scan_data: list) -> tuple:
+def parse(  # pylint: disable=too-many-locals, too-many-branches
+    scan_data: list,
+) -> tuple:
     """
     Parse chat list
 
@@ -51,19 +54,22 @@ def parse(scan_data: list) -> tuple:
     for character_id in eve_character_ids:
         eve_character = get_or_create_character(character_id=character_id)
 
+        eve_character__alliance_id = 1
         if eve_character.alliance_id is None:
-            eve_character__alliance_id = 1
+            if eve_character__alliance_id not in counter:
+                counter[eve_character__alliance_id] = 0
 
-            if "unaffiliated" not in counter:
-                counter["unaffiliated"] = 0
+            counter[eve_character__alliance_id] += 1
 
-            counter["unaffiliated"] += 1
-            alliance_info[1] = {
-                "id": 1,
-                "name": "",
-                "ticker": "",
-                "count": counter["unaffiliated"],
-            }
+            if eve_character__alliance_id not in alliance_info:
+                alliance_info[eve_character__alliance_id] = {
+                    "id": eve_character__alliance_id,
+                    "name": "",
+                    "ticker": "",
+                    "logo": eveimageserver.alliance_logo_url(
+                        alliance_id=eve_character__alliance_id, size=32
+                    ),
+                }
         else:
             eve_character__alliance_id = eve_character.alliance_id
 
@@ -71,12 +77,22 @@ def parse(scan_data: list) -> tuple:
                 counter[eve_character__alliance_id] = 0
 
             counter[eve_character__alliance_id] += 1
-            alliance_info[eve_character__alliance_id] = {
-                "id": eve_character__alliance_id,
-                "name": eve_character.alliance_name,
-                "ticker": eve_character.alliance_ticker,
-                "count": counter[eve_character__alliance_id],
-            }
+
+            if eve_character__alliance_id not in alliance_info:
+                alliance_info[eve_character__alliance_id] = {
+                    "id": eve_character__alliance_id,
+                    "name": eve_character.alliance_name,
+                    "ticker": eve_character.alliance_ticker,
+                    "logo": eveimageserver.alliance_logo_url(
+                        alliance_id=eve_character__alliance_id, size=32
+                    ),
+                    "dotlan": dotlan.alliance_url(eve_character.alliance_name),
+                    "zkillboard": zkillboard.alliance_url(eve_character__alliance_id),
+                }
+
+        alliance_info[eve_character__alliance_id]["count"] = counter[
+            eve_character__alliance_id
+        ]
 
         if eve_character.corporation_id not in counter:
             counter[eve_character.corporation_id] = 0
@@ -86,6 +102,11 @@ def parse(scan_data: list) -> tuple:
             "id": eve_character.corporation_id,
             "name": eve_character.corporation_name,
             "ticker": eve_character.corporation_ticker,
+            "logo": eveimageserver.corporation_logo_url(
+                corporation_id=eve_character.corporation_id, size=32
+            ),
+            "dotlan": dotlan.corporation_url(eve_character.corporation_name),
+            "zkillboard": zkillboard.corporation_url(eve_character.corporation_id),
             "count": counter[eve_character.corporation_id],
             "alliance": alliance_info[eve_character__alliance_id],
         }
@@ -93,14 +114,41 @@ def parse(scan_data: list) -> tuple:
         character_info[eve_character.character_id] = {
             "id": eve_character.character_id,
             "name": eve_character.character_name,
+            "portrait": eveimageserver.character_portrait_url(
+                character_id=eve_character.character_id, size=32
+            ),
+            "evewho": evewho.character_url(eve_character.character_id),
+            "zkillboard": zkillboard.character_url(eve_character.character_id),
             "corporation": corporation_info[eve_character.corporation_id],
             "alliance": alliance_info[eve_character__alliance_id],
         }
 
+    # Clean up the dicts
+    cleaned_pilot_data = []
+    cleaned_corporation_data = []
+    cleaned_alliance_data = []
+    for (
+        character_id,  # pylint: disable=unused-variable
+        character,
+    ) in character_info.items():
+        cleaned_pilot_data.append(character)
+
+    for (
+        corporation_id,  # pylint: disable=unused-variable
+        corporation,
+    ) in corporation_info.items():
+        cleaned_corporation_data.append(corporation)
+
+    for (
+        alliance_id,  # pylint: disable=unused-variable
+        alliance,
+    ) in alliance_info.items():
+        cleaned_alliance_data.append(alliance)
+
     scan_data = {
-        "pilots": character_info,
-        "corporations": corporation_info,
-        "alliances": alliance_info,
+        "pilots": cleaned_pilot_data,
+        "corporations": cleaned_corporation_data,
+        "alliances": cleaned_alliance_data,
     }
 
     return Scan.Type.CHATLIST, scan_data
