6bed6eea7277ef9bbe47fa674e6f380d9f1702a5
Peter Pfeufer <info@ppfeufer.de>
2023-08-11 18:33:42 +0200

    [CHANGE] Moved regex patterns to their own dict
    This way it's easier to re-use them later on and add more if needed.

--- aa_intel_tool/constants.py
+++ aa_intel_tool/constants.py
@@ -14,23 +14,29 @@ from aa_intel_tool import __version__
 # All internal URLs need to start with this prefix
 INTERNAL_URL_PREFIX = "-"
 
+REGEX_PATTERN = {
+    "fleetcomp": re.compile(
+        r"(?im)^([a-zA-Z0-9 -_]{3,37})[\t](.*)[\t](.*)[\t](.*)[\t](.*)[\t]([0-5] - [0-5] - [0-5])([\t](.*))?$"  # pylint: disable=line-too-long
+    ),
+    "dscan": re.compile(r"(?im)^(\d+)[\t](.*)[\t](.*)[\t](-|(.*) (km|m|AU)) ?$"),
+    "chatlist": re.compile(r"(?im)^[a-zA-Z0-9\u0080-\uFFFF -_]{3,37}$"),
+}
+
 SUPPORTED_INTEL_TYPES = [
     {
         "name": "Fleet Composition",
         "parser": "fleetcomp",
-        "pattern": re.compile(
-            r"(?im)^([a-zA-Z0-9 -_]{3,37})[\t](.*)[\t](.*)[\t](.*)[\t](.*)[\t]([0-5] - [0-5] - [0-5])([\t](.*))?$"  # pylint: disable=line-too-long
-        ),
+        "pattern": REGEX_PATTERN["fleetcomp"],
     },
     {
         "name": "D-Scan",
         "parser": "dscan",
-        "pattern": re.compile(r"(?im)^(\d+)[\t](.*)[\t](.*)[\t](-|(.*) (km|m|AU)) ?$"),
+        "pattern": REGEX_PATTERN["dscan"],
     },
     {
         "name": "Chat List",
         "parser": "chatlist",
-        "pattern": re.compile(r"(?im)^[a-zA-Z0-9\u0080-\uFFFF -_]{3,37}$"),
+        "pattern": REGEX_PATTERN["chatlist"],
     },
 ]
 
