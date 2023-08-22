b44d36338dca3c5566288e52e4a2fbe089b20321
Peter Pfeufer <info@ppfeufer.de>
2023-08-22 21:00:48 +0200

    [FIX] Return empty jSon when no data for ajax call

--- aa_intel_tool/views/ajax.py
+++ aa_intel_tool/views/ajax.py
@@ -34,6 +34,6 @@ def get_scan_data(
         ).get()
         processed_data = scan_data.processed_data
     except ScanData.DoesNotExist:  # pylint: disable=no-member
-        processed_data = None
+        processed_data = {}
 
     return JsonResponse(data=processed_data, safe=False)
