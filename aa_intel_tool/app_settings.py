1f2ac5eef2cdc6bc53d00301ca9d8477cf371287
Peter Pfeufer <info@ppfeufer.de>
2023-10-27 22:08:00 +0200

    [REMOVE] Backwards compatibility
    Not worth the massive effort involved. people need to update their instances first …

--- aa_intel_tool/app_settings.py
+++ aa_intel_tool/app_settings.py
@@ -59,38 +59,3 @@ class AppSettings:
     INTELTOOL_DSCAN_GRID_SIZE = clean_setting(
         name="INTELTOOL_DSCAN_GRID_SIZE", default_value=10000, required_type=int
     )
-
-    @classmethod
-    def allianceauth_major_version(cls):
-        """
-        Get the major version of the current installed Alliance Auth instance
-
-        :return:
-        :rtype:
-        """
-
-        return version.parse(version=allianceauth__version).major
-
-    @classmethod
-    def template_path(cls) -> str:
-        """
-        Get template path
-
-        This is used to determine if we have Alliance Auth v4 or still v3, in which case we
-        have to fall back to the legacy templates to ensure backwards compatibility
-
-        :return:
-        :rtype:
-        """
-
-        current_aa_major = cls.allianceauth_major_version()
-        app_name = AaIntelToolConfig.name
-
-        if current_aa_major < 4:
-            logger.debug(
-                msg="Alliance Auth v3 detected, falling back to legacy templates …"
-            )
-
-            return f"{app_name}/legacy_templates"
-
-        return app_name
