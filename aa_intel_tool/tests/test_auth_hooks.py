489ce02523de71d8c9ce90bb4223e47f70e5471a
Peter Pfeufer <info@ppfeufer.de>
2023-12-26 17:54:22 +0100

    [CHANGE] Use AA framework for header

--- aa_intel_tool/tests/test_auth_hooks.py
+++ aa_intel_tool/tests/test_auth_hooks.py
@@ -36,7 +36,7 @@ class TestHooks(TestCase):
 
         cls.html_menu = f"""
             <li class="d-flex flex-wrap m-2 mb-0 me-0 mt-0 p-2 pb-0 pe-0 pt-0">
-                <i class="nav-link fas fa-clipboard-list fa-fw align-self-center me-3 "></i>
+                <i class="nav-link fa-solid fa-clipboard-list fa-fw align-self-center me-3 "></i>
                 <a class="nav-link flex-fill align-self-center" href="{reverse('aa_intel_tool:intel_tool_index')}">
                     Intel Parser
                 </a>
