879cc2ee08b9eb0e61a8f1ffe4f011358aec915c
Peter Pfeufer <info@ppfeufer.de>
2025-09-18 18:25:32 +0200

    [ADD] ESI OpenAPI client Provider

--- aa_intel_tool/__init__.py
+++ aa_intel_tool/__init__.py
@@ -6,4 +6,14 @@ App init
 from django.utils.translation import gettext_lazy as _
 
 __version__ = "2.10.0"
-__title__ = _("Intel Parser")
+__title__ = "Intel Parser"
+__title_translated__ = _("Intel Parser")
+
+__esi_compatibility_date__ = "2025-08-26"
+
+__package_name__ = "aa-intel-tool"
+__app_name__ = "aa_intel_tool"
+__app_name_verbose__ = "AA Intel Tool"
+__app_name_useragent__ = "AA-Intel-Tool"
+
+__github_url__ = f"https://github.com/ppfeufer/{__package_name__}"
