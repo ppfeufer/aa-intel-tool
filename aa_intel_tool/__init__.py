8687b8cc9ec5ff426fecdc7faad7b0d99722fb8a
Peter Pfeufer <info@ppfeufer.de>
2025-09-18 18:25:32 +0200

    [ADD] ESI OpenAPI client Provider

--- aa_intel_tool/__init__.py
+++ aa_intel_tool/__init__.py
@@ -8,3 +8,12 @@ from django.utils.translation import gettext_lazy as _
 __version__ = "2.10.1"
 __title__ = "Intel Parser"
 __title_translated__ = _("Intel Parser")
+
+__esi_compatibility_date__ = "2025-08-26"
+
+__package_name__ = "aa-intel-tool"
+__app_name__ = "aa_intel_tool"
+__app_name_verbose__ = "AA Intel Tool"
+__app_name_useragent__ = "AA-Intel-Tool"
+
+__github_url__ = f"https://github.com/ppfeufer/{__package_name__}"
---
+++ aa_intel_tool/providers.py
@@ -0,0 +1,24 @@
+"""
+Providers
+"""
+
+# Alliance Auth
+from esi.openapi_clients import ESIClientProvider
+
+# AA Intel Tool
+from aa_intel_tool import (
+    __app_name_useragent__,
+    __esi_compatibility_date__,
+    __github_url__,
+    __version__,
+)
+
+# ESI client
+esi = ESIClientProvider(
+    # Use the latest compatibility date, see https://esi.evetech.net/meta/compatibility-dates
+    compatibility_date=__esi_compatibility_date__,
+    # User agent for the ESI client
+    ua_appname=__app_name_useragent__,
+    ua_version=__version__,
+    ua_url=__github_url__,
+)
