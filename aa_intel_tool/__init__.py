f0c427cf7b0f112106f56a55e2e4504f7b818e9f
Peter Pfeufer <info@ppfeufer.de>
2025-11-09 22:44:36 +0100

    [RELEASE] v2.11.0
    ## [2.11.0] - 2025-11-09
    
    ### Changed
    
    - Switched to DataTables 2
    - Use Bootstrap class instead of own styling for table width
    - Use explicitly translated title as menu item

--- aa_intel_tool/__init__.py
+++ aa_intel_tool/__init__.py
@@ -5,6 +5,6 @@ App init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "2.10.1"
+__version__ = "2.11.0"
 __title__ = "Intel Parser"
 __title_translated__ = _("Intel Parser")
