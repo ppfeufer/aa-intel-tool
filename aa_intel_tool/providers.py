c382cedb4c0e05c1de7fd67d22cc0ca9ca88de89
Peter Pfeufer <info@ppfeufer.de>
2026-02-21 18:09:58 +0100

    [ADD] ESI client provider

--- aa_intel_tool/providers.py
+++ aa_intel_tool/providers.py
@@ -5,6 +5,35 @@ Providers for the AA Intel Tool app.
 # Standard Library
 import logging
 
+# Alliance Auth
+from esi.openapi_clients import ESIClientProvider
+
+# AA Intel Tool
+from aa_intel_tool import (
+    __app_name_verbose__,
+    __esi_compatibility_date__,
+    __github_url__,
+    __version__,
+)
+
+# ESI client
+esi = ESIClientProvider(
+    # ESI compatibility date, see https://esi.evetech.net/meta/compatibility-dates
+    compatibility_date=__esi_compatibility_date__,
+    # User agent for the ESI client
+    ua_appname=__app_name_verbose__,
+    ua_version=__version__,
+    ua_url=__github_url__,
+    operations=[
+        "DeleteFleetsFleetIdMembersMemberId",
+        "GetCharactersCharacterIdFleet",
+        "GetFleetsFleetIdMembers",
+        "PostFleetsFleetIdMembers",
+        "PostUniverseNames",
+        "PutFleetsFleetId",
+    ],
+)
+
 
 class AppLogger(logging.LoggerAdapter):
     """
