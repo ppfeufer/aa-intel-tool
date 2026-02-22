97950c48f304675394dc48df9ae1a3996f6414f0
Peter Pfeufer <info@ppfeufer.de>
2026-02-22 13:12:34 +0100

    [ADD] ESI Handler class to providers

--- aa_intel_tool/providers.py
+++ aa_intel_tool/providers.py
@@ -4,15 +4,23 @@ Providers for the AA Intel Tool app.
 
 # Standard Library
 import logging
+from typing import Any
+
+# Third Party
+from aiopenapi3 import ContentTypeError
+from httpx import Response
 
 # Alliance Auth
-from esi.openapi_clients import ESIClientProvider
+from allianceauth.services.hooks import get_extension_logger
+from esi.exceptions import HTTPClientError, HTTPNotModified
+from esi.openapi_clients import ESIClientProvider, EsiOperation
 
 # AA Intel Tool
 from aa_intel_tool import (
     __app_name_verbose__,
     __esi_compatibility_date__,
     __github_url__,
+    __title__,
     __version__,
 )
 
@@ -24,15 +32,71 @@ esi = ESIClientProvider(
     ua_appname=__app_name_verbose__,
     ua_version=__version__,
     ua_url=__github_url__,
-    operations=[
-        "DeleteFleetsFleetIdMembersMemberId",
-        "GetCharactersCharacterIdFleet",
-        "GetFleetsFleetIdMembers",
-        "PostFleetsFleetIdMembers",
-        "PostUniverseNames",
-        "PutFleetsFleetId",
-    ],
+    operations=["PostCharactersAffiliation", "PostUniverseIds"],
 )
+
+
+class ESIHandler:
+    """
+    Handler for ESI operations, providing a method to retrieve results while handling exceptions.
+    """
+
+    @staticmethod
+    def result(
+        operation: EsiOperation,
+        use_etag: bool = True,
+        return_response: bool = False,
+        force_refresh: bool = False,
+        use_cache: bool = True,
+        **extra,
+    ) -> tuple[Any, Response] | Any:
+        """
+        Retrieve the result of an ESI operation, handling HTTPNotModified exceptions.
+
+        :param operation: The ESI operation to execute.
+        :type operation: EsiOperation
+        :param use_etag: Whether to use ETag for caching.
+        :type use_etag: bool
+        :param return_response: Whether to return the full response object.
+        :type return_response: bool
+        :param force_refresh: Whether to force a refresh of the data.
+        :type force_refresh: bool
+        :param use_cache: Whether to use cached data.
+        :type use_cache: bool
+        :param extra: Additional parameters to pass to the operation.
+        :type extra: dict
+        :return: The result of the ESI operation, optionally with the response object.
+        :rtype: tuple[Any, Response] | Any
+        """
+
+        logger.debug(f"Handling ESI operation: {operation.operation.operationId}")
+
+        try:
+            esi_result = operation.result(
+                use_etag=use_etag,
+                return_response=return_response,
+                force_refresh=force_refresh,
+                use_cache=use_cache,
+                **extra,
+            )
+        except HTTPNotModified:
+            logger.debug(
+                f"ESI returned 304 Not Modified for operation: {operation.operation.operationId} - Skipping update."
+            )
+
+            esi_result = None
+        except ContentTypeError:
+            logger.warning(
+                msg="ESI returned gibberish (ContentTypeError) - Skipping update."
+            )
+
+            esi_result = None
+        except HTTPClientError as exc:
+            logger.error(msg=f"Error while fetching data from ESI: {str(exc)}")
+
+            esi_result = None
+
+        return esi_result
 
 
 class AppLogger(logging.LoggerAdapter):
@@ -70,3 +134,6 @@ class AppLogger(logging.LoggerAdapter):
         """
 
         return f"[{self.prefix}] {msg}", kwargs
+
+
+logger = AppLogger(my_logger=get_extension_logger(name=__name__), prefix=__title__)
--- aa_intel_tool/tests/test_providers.py
+++ aa_intel_tool/tests/test_providers.py
@@ -4,9 +4,17 @@ Test for the providers module.
 
 # Standard Library
 import logging
+from http import HTTPStatus
+from unittest.mock import MagicMock
+
+# Third Party
+from aiopenapi3 import ContentTypeError
+
+# Alliance Auth
+from esi.exceptions import HTTPClientError, HTTPNotModified
 
 # AA Intel Tool
-from aa_intel_tool.providers import AppLogger
+from aa_intel_tool.providers import AppLogger, ESIHandler
 from aa_intel_tool.tests import BaseTestCase
 
 
@@ -96,3 +104,107 @@ class TestAppLogger(BaseTestCase):
             app_logger.info("")
 
         self.assertIn("[PREFIX] ", log.output[0])
+
+
+class TestESIHandler(BaseTestCase):
+    """
+    Test the ESIHandler provider.
+    """
+
+    def test_handles_successful_operation(self):
+        """
+        Test that a successful ESI operation returns the expected result.
+
+        :return:
+        :rtype:
+        """
+
+        mock_operation = MagicMock()
+        mock_operation.result.return_value = "success"
+
+        response = ESIHandler.result(mock_operation)
+
+        self.assertEqual(response, "success")
+        mock_operation.result.assert_called_once()
+
+    def test_handles_http_not_modified_exception(self):
+        """
+        Test that an HTTPNotModified exception is handled correctly.
+
+        :return:
+        :rtype:
+        """
+
+        mock_operation = MagicMock()
+        mock_operation.result.side_effect = HTTPNotModified(
+            status_code=HTTPStatus.NOT_MODIFIED, headers={}
+        )
+
+        response = ESIHandler.result(mock_operation)
+
+        self.assertIsNone(response)
+        mock_operation.result.assert_called_once()
+
+    def test_handles_content_type_error(self):
+        """
+        Test that a ContentTypeError exception is handled correctly.
+
+        :return:
+        :rtype:
+        """
+
+        mock_operation = MagicMock()
+        mock_response = MagicMock()
+        mock_operation.result.side_effect = ContentTypeError(
+            operation=mock_operation,
+            content_type="application/json",
+            message="Invalid content type",
+            response=mock_response,
+        )
+
+        response = ESIHandler.result(mock_operation)
+
+        self.assertIsNone(response)
+        mock_operation.result.assert_called_once()
+
+    def test_returns_none_when_http_client_error_occurs(self):
+        """
+        Test that an HTTPClientError exception is raised correctly.
+
+        :return:
+        :rtype:
+        """
+
+        mock_operation = MagicMock()
+        mock_operation.result.side_effect = HTTPClientError(
+            HTTPStatus.BAD_REQUEST, headers={}, data={}
+        )
+
+        response = ESIHandler.result(mock_operation)
+
+        self.assertIsNone(response)
+        mock_operation.result.assert_called_once()
+
+    def test_passes_extra_parameters_to_operation(self):
+        """
+        Test that extra parameters are passed correctly to the ESI operation.
+
+        :return:
+        :rtype:
+        """
+
+        mock_operation = MagicMock()
+        mock_operation.result.return_value = "success"
+
+        response = ESIHandler.result(
+            mock_operation, use_etag=False, extra_param="value"
+        )
+
+        self.assertEqual(response, "success")
+        mock_operation.result.assert_called_once_with(
+            use_etag=False,
+            return_response=False,
+            force_refresh=False,
+            use_cache=True,
+            extra_param="value",
+        )
